f109ce6eaf6e0f0dfd9ba55e736bd5509358bb7e
Thomas Hoang <megatunger@gmail.com>
2022-05-17 19:08:20 +0700

    :rocket:

---
+++ programs/maius-core/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod maius_core {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
